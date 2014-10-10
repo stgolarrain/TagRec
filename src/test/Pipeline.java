6facead323a480307e97c0053cddf6b49c8f5a69
Stgo Larrain <stgo.larrain@gmail.com>
2014-10-09 21:54:20 -0300

    .gitignore modifications and read bookmarks for title and description

--- src/test/Pipeline.java
+++ src/test/Pipeline.java
@@ -28,6 +28,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import common.Bookmark;
 import common.CalculationType;
 import common.Features;
 import processing.ActCalculator;
